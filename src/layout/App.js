d1f5fa7715878af3a998bd62f73f0367e03dcd3a
michaelx37 <michaelx2837@gmail.com>
2021-07-01 22:27:07 +0200

    moved navbar to app.js

--- src/layout/App.js
+++ src/layout/App.js
@@ -1,6 +1,7 @@
-import React /*, { Component }*/ from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+import NavBar from "../components/UI/NavBar[NEW]/NavBar";
 import Home from "../pages/Home/Home";
 import Feed from "../pages/Feed/Feed";
 import Network from "../pages/Network/Network";
@@ -11,14 +12,12 @@ import RegistrationPage from "../pages/RegistrationPage/RegistrationPage";
 import UserProfileExternal from "../pages/Profiles/UserProfileExternal";
 import TeamProfileExternal from "../pages/Profiles/TeamProfileExternal";
 
-//import NavBar from "../components/UI/NavBar/NavBar";
-
 import "./App.css";
 
 const App = () => {
   return (
     <div className="app">
-      {/* <NavBar></NavBar> */}
+      <NavBar />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/feed" exact component={Feed} />
@@ -33,24 +32,5 @@ const App = () => {
     </div>
   );
 };
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="app">
-//         <Switch>
-//           <Route path="/" exact component={Home} />
-//           <Route path="/feed" exact component={Feed} />
-//           <Route path="/search" component={Search} />
-//           <Route path="/about-us" component={AboutUs} />
-//           <Route path="/auth" exact component={LoginPage} />
-//           <Route path="/registration-page" component={RegistrationPage} />
-//           <Route path="/user-profile" component={UserProfileExternal} />
-//           <Route path="/team-profile" component={TeamProfileExternal} />
-//         </Switch>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
